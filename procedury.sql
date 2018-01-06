48c04d3a30eefe7b4f446920d13ca6a49e3eec57
zaqs114 <zaqs114@gmail.com>
2018-01-06 18:41:47 +0100

    new procedure

--- procedury.sql
+++ procedury.sql
@@ -6,15 +6,22 @@ CREATE PROCEDURE Wstaw_klienta (
 		@nip VARCHAR(64),
 		@nazwa_firmy VARCHAR(64))
 AS
-BEGIN
-                RAISERROR ('Wprowadziles bledne dane. Prosze, sprawdz je.',
-                        16,
-                        1
-                )
-END
+
 INSERT INTO klient (login, haslo, imie, nazwisko, nip, nazwa_firmy)
         VALUES (@login, @haslo, @imie, @nazwisko, @nip, @nazwa_firmy)
 
 GO
 -- Wstaw_klienta 'test1', 'test2', 'test3', 'test4', '12345678910', 'test6'
 
+CREATE PROCEDURE Usun_klienta (
+		@login VARCHAR(64)
+		)
+AS
+IF NOT EXISTS (SELECT klient_login 
+                                                FROM zamowienie
+                                                WHERE klient_login = @login)
+BEGIN
+	DELETE FROM klient
+	WHERE login = @login
+END
+GO
