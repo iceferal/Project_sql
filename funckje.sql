3e4313a5045559c5480051a17d049b3621f3f9f9
zaqs114 <zaqs114@gmail.com>
2018-01-11 11:27:12 +0100

    new function

---
+++ funckje.sql
@@ -0,0 +1,24 @@
+--wyświetla nazwy produktow podanego producenta. jesli takowego nie ma/jest bledny, wyswietla liste wszystkich producentow
+
+CREATE FUNCTION ListaProduktow
+(
+    @nazwa VARCHAR(64)
+)
+    RETURNS @produkty TABLE(nazwa VARCHAR(64))
+AS
+BEGIN
+    IF @nazwa IN (SELECT producent FROM produkt)
+        INSERT INTO @produkty
+                SELECT nazwa_produktu
+                FROM   produkt
+                WHERE  producent = @nazwa;
+    ELSE
+        INSERT INTO @produkty
+                SELECT DISTINCT producent
+                FROM   produkt;
+        RETURN;
+END;
+
+--SELECT * FROM ListaProduktow('Bosh')
+--SELECT * FROM ListaProduktow('OnePlus')
+
