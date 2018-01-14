e7a400cdf16baf61e3a0e54df305fb693957113a
iceferal <iceferal@gmail.com>
2018-01-14 23:37:59 +0100

    Create triggers.sql

---
+++ triggers.sql
@@ -0,0 +1,15 @@
+--- dziala i zapierdala
+
+Create Trigger tri_dostawa On Zamowienie After Insert
+As
+	If (Select SUM(wartosc_brutto) From Faktury
+		Where zamowienie_nr = (Select MAX(nr_zamowienia) From Zamowienie
+		Where (Month(GETDATE()) = Month(data_zlozenia) AND DAY(GETDATE()) = DAY(data_zlozenia)))) > 300
+	Declare @nr INTEGER
+	Set @nr = (Select MAX(nr_zamowienia) From Zamowienie
+		Where (Month(GETDATE()) = Month(data_zlozenia) AND DAY(GETDATE()) = DAY(data_zlozenia)))
+
+	Update Zamowienie
+	Set koszt_dostawy = 0
+	Where nr_zamowienia = @nr
+Go
