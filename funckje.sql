2fe6548fc0a152036f4c24ed274e8fbcf238cb62
zaqs114 <zaqs114@gmail.com>
2018-01-18 14:08:48 +0100

    new view and function added

--- funckje.sql
+++ funckje.sql
@@ -23,6 +23,28 @@ END;
 --SELECT * FROM ListaProduktow('OnePlus')
 
 
+--zwraca liste zamowien danego klienta
+CREATE FUNCTION ZamowieniaKlienta(
+	@login VARCHAR(64)
+	)
+RETURNS TABLE
+AS
+RETURN 
+SELECT nr_zamowienia,
+		login,
+		cena_brutto 
+FROM klient k
+JOIN zamowienie z
+ON z.klient_login=k.login
+JOIN produktZamowienie PR
+ON Pr.zamowienie_nr=z.id_zamowienia
+JOIN produkt p
+ON p.kod_produktu=PR.produkt_kod_produktu
+WHERE k.login=@login
+
+--SELECT * FROM ZamowieniaKlienta('Alim32')
+
+
 --nie dziala jak powinno
 CREATE FUNCTION LiczbaZamowien
 (
@@ -30,10 +52,25 @@ CREATE FUNCTION LiczbaZamowien
 )
     RETURNS INT
 AS
-BEGIN
 	DECLARE @wynik INT
 	SELECT @wynik= COUNT(z.nr_zamowienia)
 	FROM zamowienie Z
 	WHERE z.klient_login = @login
+BEGIN
 	RETURN @wynik
 END;
+
+--zwraca totalna cene zamowienia
+CREATE FUNCTION CenaZamowienia
+(
+	@numer INT
+)
+RETURNS MONEY
+AS
+BEGIN
+DECLARE @suma MONEY
+set @suma = (SELECT SUM(cena_brutto) FROM WszystkieZamowienia)
+RETURN @suma
+END;
+
+--SELECT dbo.CenaZamowienia(1)
--- widoki.sql
+++ widoki.sql
@@ -19,3 +19,22 @@ AS
 GO
 
 --SELECT * FROM Dane_Klientow
+
+CREATE VIEW WszystkieZamowienia(nr_zamowienia, login, nazwa_produktu, cena_netto, cena_brutto)
+AS
+(SELECT z.nr_zamowienia,
+		k.login,
+		p.nazwa_produktu ,
+		p.cena_netto,
+		p.cena_brutto
+FROM klient k
+JOIN zamowienie z
+ON z.klient_login=k.login
+JOIN produktZamowienie PR
+ON Pr.zamowienie_nr=z.id_zamowienia
+JOIN produkt p
+ON p.kod_produktu=PR.produkt_kod_produktu
+)
+GO
+
+--SElect * from WszystkieZamowienia
